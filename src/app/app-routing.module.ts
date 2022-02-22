50b7a89dcba193251690f6ae1b41aacd263f8506
Saumyadip Biswas <72658964+Assassinationrogue@users.noreply.github.com>
2022-02-22 18:57:04 +0530

    # inital commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { LandingPageComponent } from './landing-page/landing-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,37 @@
 import { NgModule } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { ContactListComponent } from './components/contact-list/contact-list.component';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import {  ButtonModule } from 'primeng/button';
+
+import { EditFormComponent } from './components/edit-form/edit-form.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PopupDirective } from './directives/popup.directive';
+import { PopupComponent } from './directives/popup/popup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ContactListComponent,
+    EditFormComponent,
+    PopupDirective,
+    PopupComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule,
+    DynamicDialogModule,
+    BrowserAnimationsModule,
+    ButtonModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
